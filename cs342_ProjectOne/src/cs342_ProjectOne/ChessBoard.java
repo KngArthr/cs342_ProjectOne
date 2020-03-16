838cc316f80b32c276c2ba33607562afbaf486a3
KngArthr <56852100+KngArthr@users.noreply.github.com>
2020-03-16 09:32:22 -0400

    working on main parts

--- cs342_ProjectOne/src/cs342_ProjectOne/ChessBoard.java
+++ cs342_ProjectOne/src/cs342_ProjectOne/ChessBoard.java
@@ -39,6 +39,39 @@ public class ChessBoard {
 		return MAX_COLUMN;
 	}
 ///////////////////////////////////////////////////////////////////
+/// isConflictStack///
+/// Input : stack///
+/// Output: true or false ///
+/// returns true or false based on if any queens in the stack conflict
+/// with each other.
+/// ///
+///////////////////////////////////////////////////////////////////
+	public boolean isConflictStack(LinkedStack queenStack) {
+		
+		Queen queen1;//two variables to compare 1 queen with all other queens in the stack
+		Queen queen2;//for the first iteration, queen 1 will stay the same while queen 2 will cycle through the stack
+		//next iteration will move to next queen in stack, and queen 2 will cycle again through the stack
+		for(int i = 0; i < queenStack.size(); i++) {//queen1 will be what the other queens are compared against
+			queen1 = queenStack.readStack(i);//i is the queen that is compared against
+			
+			for(int j = 0; j < queenStack.size(); j++) {//queen 2 will be the rest of the queens in the stack
+				queen2 = queenStack.readStack(j);//j will be the rest of the queens
+				if(i == j) {//if i == j then it is the same slot in the stack and does not need to do anything
+					
+					//do nothing
+				}else if(isConflictHorVer(queen1, queen2)||isConflictDiagonal(queen1, queen2)) {
+					return true;//if there is a horizontal, vertical, or diagonal conflict then return true for conflict.
+				}
+				
+				
+			}
+		}
+		
+		return false ;//return false if there are no conflicts
+	}
+	
+
+///////////////////////////////////////////////////////////////////
 /// isConflictHorVer///
 /// Input : two queens///
 /// Output: true or false ///
@@ -69,7 +102,28 @@ public class ChessBoard {
 			counter++;
 			
 			if(queen1.getRow() + counter > -1 && queen1.getRow() + counter < getMAX_ROW() && queen1.getColumn() + counter > -1 && queen1.getColumn() + counter < getMAX_COLUMN()) {
-				
+				if(queen1.getRow() + counter == queen2.getRow() && queen1.getColumn() + counter == queen2.getColumn()){
+					return true;
+				}
+
+			}
+			if(queen1.getRow() - counter > -1 && queen1.getRow() - counter < getMAX_ROW() && queen1.getColumn() - counter > -1 && queen1.getColumn() - counter < getMAX_COLUMN()) {
+				if(queen1.getRow() - counter == queen2.getRow() && queen1.getColumn() - counter == queen2.getColumn()){
+					return true;
+				}
+
+			}
+			if(queen1.getRow() + counter > -1 && queen1.getRow() + counter < getMAX_ROW() && queen1.getColumn() - counter > -1 && queen1.getColumn() - counter < getMAX_COLUMN()) {
+				if(queen1.getRow() + counter == queen2.getRow() && queen1.getColumn() - counter == queen2.getColumn()){
+					return true;
+				}
+
+			}
+			if(queen1.getRow() - counter > -1 && queen1.getRow() - counter < getMAX_ROW() && queen1.getColumn() + counter > -1 && queen1.getColumn() + counter < getMAX_COLUMN()) {
+				if(queen1.getRow() - counter == queen2.getRow() && queen1.getColumn() + counter == queen2.getColumn()){
+					return true;
+				}
+
 			}
 			
 			
@@ -78,7 +132,45 @@ public class ChessBoard {
 		
 		return false;
 	}
+
+///////////////////////////////////////////////////////////////////
+/// printChessBoard///
+/// Input : stack///
+/// Output: printed chessboard with queens from stack ///
+/// peints a chessboard to the console with Queens from stack
+/// ///
+///////////////////////////////////////////////////////////////////
 	
-	
+	public void printChessBoard(LinkedStack queenStack) {
+		System.out.print("+---+---+---+---+---+---+---+---+\n");
+		
+		for(int i = 0; i < getMAX_ROW(); i++) {
+			System.out.print("|");
+			for(int j = 0; j < getMAX_COLUMN(); j++) {
+				System.out.print(" ");
+				for(int k = 0; k < queenStack.size(); k++) {
+					if((queenStack.readStack(k).getRow()==i) && queenStack.readStack(k).getColumn() == j) {
+						System.out.print("Q");
+						break;
+					}else if(k == queenStack.size()-1) {
+						System.out.print(" ");
+					}
+					
+				}
+				System.out.print(" ");
+				System.out.print("|");
+				if(j == getMAX_COLUMN()-1) {
+					System.out.print("\n");
+
+				}
+
+
+			}
+			System.out.print("+---+---+---+---+---+---+---+---+\n");
+			
+			
+		}
+		
+	}
 
 }
--- cs342_ProjectOne/src/cs342_ProjectOne/Driver.java
+++ cs342_ProjectOne/src/cs342_ProjectOne/Driver.java
@@ -59,12 +59,56 @@ public class Driver {
 		
 		ChessBoard chessBoard = new ChessBoard(8, 8);
 		
-		Queen queen1 = new Queen(67, 2);
-		Queen queen2 = new Queen(35, 2);
+		boolean isSuccess = false;
+		
+		int counter = 0;
+		
+		while(counter < 9||!queenStack.isEmpty()) {
+			if(counter > 8){
+				break;
+			}else {
+				counter++;
+			}
+			
+			for(int i = 0; i < chessBoard.getMAX_ROW(); i++) {
+				for(int j = 0; j < chessBoard.getMAX_COLUMN(); j++) {
+					Queen queen = new Queen(i, j);
+					queenStack.push(queen);
+					
+					isSuccess = !chessBoard.isConflictStack(queenStack);//success if there are no conflicts in the stack
+					
+					System.out.println("isSuccess?: " + isSuccess);
+					System.out.println("Counter: " + counter);
+					System.out.println("I is: " + i + " and J is: " + j);
+
+					System.out.println(queenStack);
+					System.out.println(counter);
+
+					chessBoard.printChessBoard(queenStack);
+
+
+					if(isSuccess == false) {
+						queenStack.pop();// if there is no success then pop the last added queen to remove the conflict
+					}else {
+						break;
+					}
+
+				}
+				if(isSuccess == true) {
+					break;
+				}
+				
+
+			}
+			
+		}
+		
+		//Queen queen1 = new Queen(1, 2);
+		//Queen queen2 = new Queen(5, 6);
 
 		
-		queenStack.push(queen1);
-		queenStack.push(queen2);
+		//queenStack.push(queen1);
+		//queenStack.push(queen2);
 		
 		//System.out.println(queenStack.peek());
 
@@ -75,8 +119,9 @@ public class Driver {
 		//System.out.println(queenStack.readStack(0));
 
 		System.out.println(queenStack);
+		chessBoard.printChessBoard(queenStack);
 		
-		
+		//System.out.println(chessBoard.isConflictStack(queenStack));
 		
 		
 		
